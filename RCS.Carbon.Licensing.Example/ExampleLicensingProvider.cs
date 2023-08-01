0ce40504fb04376d0af7d53b4bdacc7e5e394206
Greg Keogh <gfkeogh@gmail.com>
2023-08-01 11:49:45 +1000

    1.0.6 Update to Carbon 8.5.21.

--- RCS.Carbon.Licensing.Example/ExampleLicensingProvider.cs
+++ RCS.Carbon.Licensing.Example/ExampleLicensingProvider.cs
@@ -7,18 +7,17 @@ using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Reflection;
-using System.Reflection.PortableExecutable;
 using System.Security.Cryptography;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using Azure;
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
 using Microsoft.EntityFrameworkCore;
 using RCS.Carbon.Licensing.Example.EFCore;
 using RCS.Carbon.Licensing.Shared;
 using RCS.Carbon.Shared;
-using static Azure.Core.HttpHeader;
 
 
 #nullable enable
@@ -33,7 +32,7 @@ public class ExampleLicensingProvider : ILicensingProvider
 {
 	readonly string? _connect;
 	const string GuestAccountName = "guest";
-	string mimeurl = "https://systemrcs.blob.core.windows.net/reference/mime-types.xml";
+	const string MimeUrl = "https://systemrcs.blob.core.windows.net/reference/mime-types.xml";
 
 	[Description("Example licensing provider using a SQL Server database")]
 	public ExampleLicensingProvider(
@@ -161,7 +160,7 @@ public class ExampleLicensingProvider : ILicensingProvider
 				JobIds = c.Jobs.Select(j => j.Id.ToString()).ToArray(),
 				UserIds = c.Users.Select(u => u.Id.ToString()).ToArray()
 			}
-			).ToArrayAsync();
+			).ToArrayAsync().ConfigureAwait(false);
 		var jobs = await context.Jobs.AsNoTracking()
 			.Include(j => j.Users)
 			.AsAsyncEnumerable()
@@ -175,7 +174,7 @@ public class ExampleLicensingProvider : ILicensingProvider
 				UserIds = j.Users.Select(u => u.Id.ToString()).ToArray(),
 				VartreeNames = j.VartreeNames?.Split(',') ?? Array.Empty<string>()
 			}
-			).ToArrayAsync();
+			).ToArrayAsync().ConfigureAwait(false);
 		var users = await context.Users.AsNoTracking()
 			.Include(u => u.Customers)
 			.Include(u => u.Jobs)
@@ -189,7 +188,7 @@ public class ExampleLicensingProvider : ILicensingProvider
 				CustomerIds = u.Customers.Select(c => c.Id.ToString()).ToArray(),
 				JobIds = u.Jobs.Select(j => j.Id.ToString()).ToArray()
 			}
-			).ToArrayAsync();
+			).ToArrayAsync().ConfigureAwait(false);
 		return new Shared.Entities.NavData()
 		{
 			Customers = custs,
@@ -198,7 +197,7 @@ public class ExampleLicensingProvider : ILicensingProvider
 		};
 	}
 
-	public async Task<string> GetNavigationXml()
+	public async Task<XElement> GetNavigationXml()
 	{
 		using var context = MakeContext();
 		var elem = new XElement("Navigation");
@@ -228,7 +227,7 @@ public class ExampleLicensingProvider : ILicensingProvider
 			new XElement("Jobs", jobs),
 			new XElement("Users", users)
 		);
-		return elem.ToString();
+		return elem;
 	}
 
 	#endregion
@@ -743,7 +742,7 @@ public class ExampleLicensingProvider : ILicensingProvider
 		{
 			using (var client = new HttpClient())
 			{
-				string xml = await client.GetStringAsync(mimeurl);
+				string xml = await client.GetStringAsync(MimeUrl);
 				mimedoc = XDocument.Parse(xml);
 			}
 		}
@@ -847,6 +846,83 @@ public class ExampleLicensingProvider : ILicensingProvider
 	}
 
 	#endregion
+
+	public async Task<XElement> RunComparison()
+	{
+		var elem = new XElement("Comparison");
+		var context = MakeContext();
+		// Outer loop over storage accounts as database customers
+		await foreach (var cust in context.Customers.AsNoTracking().Include(c => c.Jobs).AsAsyncEnumerable())
+		{
+			var conlist = new List<BlobContainerItem>();
+			var celem = new XElement("Customer", new XAttribute("Name", cust.Name));
+			elem.Add(celem);
+			try
+			{
+				// List the cloud containers
+				var client = new BlobServiceClient(cust.StorageKey);
+				string? token = null;
+				do
+				{
+					var pages = client.GetBlobContainersAsync(traits: BlobContainerTraits.Metadata).AsPages(token);
+					await foreach (Azure.Page<BlobContainerItem> page in pages)
+					{
+						foreach (BlobContainerItem con in page.Values)
+						{
+							conlist.Add(con);
+						}
+					}
+				}
+				while (token?.Length > 0);
+				// Compare the licensing job rows and the containers
+				BlobContainerItem[] matches = conlist.Where(c => cust.Jobs.Any(j => j.Name == c.Name)).ToArray();
+				BlobContainerItem[] cononly = conlist.Where(c => !cust.Jobs.Any(j => j.Name == c.Name)).ToArray();
+				Job[] jobonly = cust.Jobs.Where(j => !conlist.Any(c => c.Name == j.Name)).ToArray();
+				var elems1 = matches.Select(m => new XElement("Job", new XAttribute("Name", m.Name), new XAttribute("State", (int)JobState.OK)));
+				var elems2 = cononly.Select(c => new XElement("Job", new XAttribute("Name", c.Name), new XAttribute("State", (int)JobState.OrphanContainer)));
+				var elems3 = jobonly.Select(j => new XElement("Job", new XAttribute("Id", j.Id), new XAttribute("Name", j.Name), new XAttribute("State", (int)JobState.OrphanJobRecord)));
+				var jobelems = elems1.Concat(elems2).Concat(elems3).OrderBy(e => (string)e.Attribute("Name")!);
+				celem.Add(jobelems);
+			}
+			catch (Exception ex)
+			{
+				// Storage account access probably failed due to a bad connection string
+				string msg = ex.Message.Split("\r\n".ToCharArray()).First();
+				celem.Add(new XElement("Error", new XAttribute("Type", ex.GetType().Name), msg));
+			}
+		}
+		return elem;
+	}
+
+	public async IAsyncEnumerable<BlobData> ListJobBlobs(string customerName, string jobName)
+	{
+		var context = MakeContext();
+		var cust = await context.Customers.AsNoTracking().Include(c => c.Jobs).FirstOrDefaultAsync(c => c.Name == customerName);
+		if (cust != null)
+		{
+			var client = new BlobServiceClient(cust.StorageKey);
+			var cc = client.GetBlobContainerClient(jobName);
+			if (await cc.ExistsAsync())
+			{
+				string? token = null;
+				do
+				{
+					IAsyncEnumerable<Page<BlobItem>> pages = cc.GetBlobsAsync().AsPages(token);
+					await foreach (Page<BlobItem> page in pages)
+					{
+						foreach (BlobItem blob in page.Values)
+						{
+							var data = new BlobData(blob.Name, blob.Properties.ContentLength!.Value, blob.Properties.CreatedOn!.Value.UtcDateTime, blob.Properties.LastModified?.UtcDateTime, blob.Properties.ContentType);
+							yield return data;
+						}
+						token = page.ContinuationToken;
+					}
+				}
+				while (token?.Length > 0);
+			}
+		}
+
+	}
 
 	#region Row To Entities
 
